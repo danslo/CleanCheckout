6cb970ed919538c5534031def4751e027b39fede
Daniel Sloof <goapsychadelic@gmail.com>
2017-10-15 22:37:22 +0200

    remove useless stuff to reduce step navigation jerkiness

--- view/frontend/web/js/model/navigator-mixin.js
+++ view/frontend/web/js/model/navigator-mixin.js
@@ -5,40 +5,24 @@ define([
     'use strict';
 
     /**
-     * Disallow going back to Welcome step when we're logged in.
+     * - Disallow going back to Welcome step when we're logged in.
+     * - Stop jerky animations between steps by removing body animations.
      */
     return function (target) {
         target.navigateTo = function (code, scrollToElementId) {
             if (customer.isLoggedIn() && code === 'welcome-step') {
                 return;
             }
-
-            var sortedItems = target.steps.sort(this.sortItems),
-                bodyElem = $.browser.safari || $.browser.chrome ? $('body') : $('html');
-
-            scrollToElementId = scrollToElementId || null;
-
+            var sortedItems = target.steps.sort(this.sortItems);
             if (!this.isProcessed(code)) {
                 return;
             }
             sortedItems.forEach(function (element) {
                 if (element.code === code) {
                     element.isVisible(true);
-                    bodyElem.animate({
-                        scrollTop: $('#' + code).offset().top
-                    }, 0, function () {
-                        window.location = window.checkoutConfig.checkoutUrl + '#' + code;
-                    });
-
-                    if (scrollToElementId && $('#' + scrollToElementId).length) {
-                        bodyElem.animate({
-                            scrollTop: $('#' + scrollToElementId).offset().top
-                        }, 0);
-                    }
                 } else {
                     element.isVisible(false);
                 }
-
             });
         };
         return target;
