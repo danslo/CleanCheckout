7a0cdf929bfe169c90a36c64dfd4662531aa5cea
Daniel Sloof <goapsychadelic@gmail.com>
2017-10-16 11:32:42 +0200

    add colorpicker

---
+++ Block/Colorpicker.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rubic\SimpleCheckout\Block;
+
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Colorpicker extends Field
+{
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $html = $element->getElementHtml();
+        $value = $element->getData('value');
+
+        $html .= '<script type="text/javascript">
+            require(["jquery","jquery/colorpicker/js/colorpicker"], function ($) {
+                $(document).ready(function () {
+                    var e = $("#' . $element->getHtmlId() . '");
+                    e.css("backgroundColor", "'. $value .'");
+                    e.ColorPicker({
+                        color: "'. $value .'",
+                        onChange: function (hsb, hex, rgb) {
+                            e.css("backgroundColor", "#" + hex).val("#" + hex);
+                        }
+                    });
+                });
+            });
+            </script>';
+        return $html;
+    }
+}
