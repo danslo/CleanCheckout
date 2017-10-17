f6d05b38d325a7cc6f41c64caab50769421f6b57
Daniel Sloof <goapsychadelic@gmail.com>
2017-10-17 19:55:32 +0200

    make things less stupid for virtual produts

--- view/frontend/web/js/view/payment-mixin.js
+++ view/frontend/web/js/view/payment-mixin.js
@@ -9,11 +9,12 @@ define(
          */
         return function (target) {
             return target.extend({
-                initialize: function () {
-                    this.visible = ko.observable(false);
-                    this._super();
-                    return this;
-                }
+                defaults: {
+                    template: 'Rubic_CleanCheckout/payment',
+                    activeMethod: ''
+                },
+
+                isVisible: ko.observable(false)
             });
         };
     }
