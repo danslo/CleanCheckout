98ce336ce3d90cd88b2d535294c44ffd9b355736
Daniel Sloof <goapsychadelic@gmail.com>
2017-10-17 19:24:16 +0200

    force luma checkout functionality

---
+++ Plugin/ForceLumaCheckoutPlugin.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Rubic\CleanCheckout\Plugin;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Design\Theme\ThemeProviderInterface;
+use Magento\Theme\Model\View\Design;
+
+class ForceLumaCheckoutPlugin
+{
+    const CONFIG_PATH_FORCE_LUMA_CHECKOUT = 'clean_checkout/general/force_luma_checkout';
+
+    /**
+     * @var ThemeProviderInterface
+     */
+    private $themeProvider;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param ThemeProviderInterface $themeProvider
+     * @param RequestInterface $request
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        ThemeProviderInterface $themeProvider,
+        RequestInterface $request
+    ) {
+        $this->themeProvider = $themeProvider;
+        $this->request = $request;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return int
+     */
+    private function getLumaThemeId()
+    {
+        $theme = $this->themeProvider->getThemeByFullPath('frontend/Magento/luma');
+        return $theme->getId();
+    }
+
+    /**
+     * Forces the Luma theme when request is being handled by checkout.
+     *
+     * @param Design $subject
+     * @param callable $proceed
+     * @param array ...$args
+     * @return int
+     */
+    public function aroundGetConfigurationDesignTheme(Design $subject, callable $proceed, ...$args)
+    {
+        $forceLuma = $this->scopeConfig->getValue(self::CONFIG_PATH_FORCE_LUMA_CHECKOUT);
+        if ($forceLuma && $this->request->getModuleName() === 'checkout') {
+            return $this->getLumaThemeId();
+        }
+        return $proceed(...$args);
+    }
+}
