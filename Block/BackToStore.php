e6537acc449273bc80a69245b31673a437130b56
Daniel Sloof <goapsychadelic@gmail.com>
2017-10-18 13:21:05 +0200

    back to store functionality

---
+++ Block/BackToStore.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Rubic\CleanCheckout\Block;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Store\App\Response\Redirect;
+
+class BackToStore extends Template
+{
+    const CONFIG_PATH_BACK_TO_STORE_LABEL = 'clean_checkout/back_to_store/label';
+
+    /**
+     * @var Redirect
+     */
+    private $redirect;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param Context $context
+     * @param ScopeConfigInterface $scopeConfig
+     * @param Redirect $redirect
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig,
+        Redirect $redirect,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->scopeConfig = $scopeConfig;
+        $this->redirect = $redirect;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBackToStoreLabel()
+    {
+        return $this->scopeConfig->getValue(self::CONFIG_PATH_BACK_TO_STORE_LABEL);
+    }
+
+    /**
+     * @return string
+     */
+    public function getBackToStoreUrl()
+    {
+        return $this->redirect->getRefererUrl();
+    }
+}
