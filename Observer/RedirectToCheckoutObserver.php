cacb5c6b4994abff3eb4f53622a5c641ee6970dd
Daniel Sloof <goapsychadelic@gmail.com>
2017-10-23 14:52:13 +0200

    allow redirect to checkout on adding product

---
+++ Observer/RedirectToCheckoutObserver.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright © 2017 Rubic. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+namespace Rubic\CleanCheckout\Observer;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\UrlInterface;
+
+class RedirectToCheckoutObserver implements ObserverInterface
+{
+    const CONFIG_PATH_REDIRECT_TO_CHECKOUT = 'clean_checkout/general/redirect_to_checkout';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var UrlInterface
+     */
+    private $url;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param UrlInterface $url
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig, UrlInterface $url)
+    {
+        $this->scopeConfig = $scopeConfig;
+        $this->url = $url;
+    }
+
+    /**
+     * Redirects directly to checkout on adding product, if we're configured to do so.
+     *
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        if (!$this->scopeConfig->getValue(self::CONFIG_PATH_REDIRECT_TO_CHECKOUT)) {
+            return;
+        }
+
+        $request = $observer->getRequest();
+        $request->setParam('return_url', $this->url->getUrl('checkout'));
+    }
+}
