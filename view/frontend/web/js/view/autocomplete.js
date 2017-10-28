a9a8ae6852036e056b6cc4775bb1dc93c808f360
Daniel Sloof <goapsychadelic@gmail.com>
2017-10-28 11:26:30 +0200

    temporary commit

--- view/frontend/web/js/view/autocomplete.js
+++ view/frontend/web/js/view/autocomplete.js
@@ -2,71 +2,100 @@
  * Copyright © 2017 Rubic. All rights reserved.
  * See LICENSE.txt for license details.
  */
-if (window.checkoutConfig.autoComplete.enabled) {
-    define(
-        [
-            'uiComponent',
-            'jquery',
-            'async!https://maps.googleapis.com/maps/api/js?key=' + window.checkoutConfig.autoComplete.apiKey + '&libraries=places&callback=initAutocomplete',
-        ],
-        function (Component, $) {
-            'use strict';
+define(
+    [
+        'uiComponent',
+        'jquery',
+        'async'
+    ],
+    function (Component, $, async) {
+        'use strict';
 
-            return Component.extend({
-                autocomplete: null,
+        return Component.extend({
+            autocomplete: null,
 
-                fields: {
-                    route: "input[name='street[0]']",
-                    street_number: "input[name='street[1]']",
-                    postal_code: "input[name='postcode']",
-                    locality: "input[name='city']",
-                    country: "select[name='country_id']"
-                },
+            fields: {
+                route:          { short_name: "input[name='street[0]']" },
+                street_number:  { short_name: "input[name='street[1]']" },
+                postal_code:    { short_name: "input[name='postcode']" },
+                locality:       { short_name: "input[name='city']" },
+                postal_town:    { short_name: "input[name='city']" },
+                country:        { short_name: "select[name='country_id']" },
+                administrative_area_level_1: { long_name: "input[name='region']:visible" }
+            },
 
-                initialize: function () {
-                    $(document).on('keypress', "input[name='street[0]']", function (e) {
-                        if (this.autocomplete === null) {
-                            this.autocomplete = new google.maps.places.Autocomplete(e.target, {types: ['geocode']});
-                            this.autocomplete.addListener('place_changed', this.fillAddressFields.bind(this));
+            initialize: function () {
+                if (window.checkoutConfig.autoComplete.enabled) {
+                    async.load(
+                        'https://maps.googleapis.com/maps/api/js?key=' + window.checkoutConfig.autoComplete.apiKey + '&libraries=places&callback=initAutocomplete',
+                        requirejs,
+                        function () {
+                            $(document).on('keypress', "input[name='street[0]']", function (e) {
+                                if (this.autocomplete === null) {
+                                    this.autocomplete = new google.maps.places.Autocomplete(e.target, {types: ['geocode']});
+                                    this.autocomplete.addListener('place_changed', this.fillAddressFields.bind(this));
+                                }
+                            }.bind(this));
+                        }.bind(this),
+                        {isBuild: false}
+                    );
+                }
+                return this._super();
+            },
+
+            findComponentValue: function (place, type, subtype) {
+                for (var i = 0; i < place.address_components.length; i++) {
+                    for (var j = 0; j < place.address_components[i].types.length; j++) {
+                        var addressType = place.address_components[i].types[j];
+                        if (addressType === type) {
+                            return place.address_components[i][subtype];
                         }
-                    }.bind(this));
-                    return this._super();
-                },
-
-                findComponentValue: function (place, type) {
-                    for (var i = 0; i < place.address_components.length; i++) {
-                        for (var j = 0; j < place.address_components[i].types.length; j++) {
-                            var addressType = place.address_components[i].types[j];
-                            if (addressType === type) {
-                                return place.address_components[i].short_name;
-                            }
-                        }
-
                     }
-                    return null;
-                },
 
-                fillAddressFields: function () {
-                    var place = this.autocomplete.getPlace();
-                    if (typeof place === 'undefined') {
-                        return;
-                    }
-                    for (var key in this.fields) {
-                        if (this.fields.hasOwnProperty(key)) {
-                            var field = $(this.fields[key]);
-                            var value = this.findComponentValue(place, key);
-                            if (value !== null) {
-                                if (field.length) {
-                                    field.val(value).change();
-                                } else if (key === 'street_number') {
-                                    // Couldn't find second address field, just append it to the address.
-                                    $(this.fields.route).val($(this.fields.route).val() + ' ' + value).change();
+                }
+                return null;
+            },
+
+            fillAddressFields: function () {
+                var place = this.autocomplete.getPlace();
+                if (typeof place === 'undefined') {
+                    return;
+                }
+                console.log(place);
+                for (var type in this.fields) {
+                    if (this.fields.hasOwnProperty(type)) {
+                        for (var subtype in this.fields[type]) {
+                            if (this.fields[type].hasOwnProperty(subtype)) {
+                                var selector = this.fields[type][subtype];
+                                var field = $(selector);
+                                var value = this.findComponentValue(place, type, subtype);
+                                if (value !== null) {
+                                    if (field.length) {
+                                        field.val(value).change();
+                                    } else if (type === 'street_number') {
+                                        // Couldn't find second address field, just append/prepend it to the address.
+                                        var routeSelector = this.fields.route.short_name;
+                                        var country = this.findComponentValue(place, 'country', 'short_name');
+                                        var routeValue = $(routeSelector).val();
+                                        if (country !== null && country === 'US') {
+                                            routeValue = value + ' ' + routeValue;
+                                        } else {
+                                            routeValue = routeValue + ' ' + value;
+                                        }
+                                        $(routeSelector).val(routeValue).change();
+                                    } else if (type === 'administrative_area_level_1') {
+                                        // Couldn't find visible region input, dealing with a dropdown.
+                                        $("select[name='region_id'] option").filter(function() {
+                                            return $(this).text() === value;
+                                        }).prop('selected', true).change();
+                                    }
                                 }
                             }
                         }
+
                     }
                 }
-            });
-        }
-    );
-}
+            }
+        });
+    }
+);
